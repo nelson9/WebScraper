270a14da05972367beb9faadf8d3a38710003135
nelson9 <nferguson@macmillan.org.uk>
2016-08-26 13:50:45 +0100

    intial commit

---
+++ WebScraper/Event.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScraper
+{
+    public class Event
+    {
+        public string Artist { get; set; }
+        public string City { get; set; }
+        public string VenueName { get; set; }
+        public DateTime Date { get; set; }
+        public Decimal Price { get; set; }
+    }
+}
---
+++ WebScraper/WebScraper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace WebScraper
+{
+    public class WebScraper
+    {
+        public void GetHtml()
+        {
+            HtmlWeb web = new HtmlWeb();
+ 
+            HtmlDocument document = web.Load("http://www.wegottickets.com/searchresults/all");
+
+            HtmlNode[] links =
+                document.DocumentNode.SelectNodes("//a")
+                    .Where(x => x.Attributes.Contains("class") && x.Attributes["class"].Value.Contains("event_link")).ToArray();
+         
+
+
+            foreach (HtmlNode item in links)
+            {
+                Console.WriteLine(item.InnerHtml); 
+            }
+
+        }
+
+    }
+}
